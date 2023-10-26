9124bb79b6bbb8f9e43550d6244fbe98aadf6b4d
Andrew Shmorhun <andrewshmorgun@gmail.com>
2023-10-26 23:11:30 +0300

    fix types

---
+++ src/app/(main)/[id]/page.tsx
@@ -0,0 +1,15 @@
+import { DynamicServerComponent } from "@/components/DynamicComponent";
+import { Tab } from "@/types";
+import { routes } from "@/utils/utils";
+import React from "react";
+
+export default function DynamicPage({ params }: { params: { id: string } }) {
+  if (params) {
+    const id = params.id;
+    const route = routes.find((route) => route.id === id) as Tab;
+    if (route !== undefined) {
+      const path = route.path;
+      return <DynamicServerComponent path={path} />;
+    }
+  }
+}
